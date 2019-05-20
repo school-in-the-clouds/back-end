2555b978100e8f4e36c9d36161bfc32edbec0aba
Brandon Allison <brandona8128@outlook.com>
2019-05-20 19:40:32 -0400

    removed profiles

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,15 @@
-// Update with your config settings.
+
 
 module.exports = {
   development: {
     client: 'sqlite3',
-    connection: { filename: './database/auth.db3' }, // change this if you want a different name for the database
+    connection: { filename: './database/auth.db3' }, 
     useNullAsDefault: true, 
+    pool: {
+      afterCreate: (conn, done) => {
+          conn.run("PRAGMA foreign_keys = ON", done);
+      }
+  },
     migrations: {
       directory: './database/migrations',
     },
@@ -14,9 +19,15 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
+    pool: {
+      afterCreate: (conn, done) => {
+          conn.run("PRAGMA foreign_keys = ON", done);
+      }
+  },
     migrations: {
       directory: './database/migrations',
     },
+    seeds: { directory: './database/seeds' },
   }
 };
 
