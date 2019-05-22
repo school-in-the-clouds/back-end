7e738f6dd7e4180735bbb648ee8de8c55e5facbb
Brandon Allison <brandona8128@outlook.com>
2019-05-22 18:30:06 -0400

    starting some tests

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const apiRoutes = require('./api/apiRoutes')
 const server = express()
-server.get('/', (req,res)=>{res.status(200).send("Here is the server")})
+server.get('/', (req,res)=>{res.status(200).json({message:"Server Working"})})
 server.use('/api', apiRoutes)
 server.use('/uploads', express.static(__dirname + '/public'))
 
