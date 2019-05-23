da7b43378dab50e1e5209ef0f3095c57a1ef67ec
Brandon Allison <brandona8128@outlook.com>
2019-05-23 12:16:52 -0400

    had a bug

--- api/apiRoutes.js
+++ api/apiRoutes.js
@@ -2,14 +2,13 @@ const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 
-const userRoutes = require('./routes/userRoutes')
+
 const accountRoutes = require('./routes/accountRoutes')
 const taskRoutes = require ('./routes/taskRoutes')
 
 const router = express.Router()
 router.use(cors())
 router.use(morgan('short'))
-router.use('/users', userRoutes)
 router.use('/tasks', taskRoutes)
 router.use('/accounts', accountRoutes)
 
