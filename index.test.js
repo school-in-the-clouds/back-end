7248ec2f12f7a904b46037fde2372c6364019917
Brandon Allison <brandona8128@outlook.com>
2019-05-24 12:57:39 -0400

    added some test

--- index.test.js
+++ index.test.js
@@ -1,11 +1,31 @@
-const server = require('.index');
-const db = require('./database/dbConfig');
+const request = require('supertest');
+const apiRoutes = require('.apiRoutes.js');
 
-
-describe('get /', () => {
-    it('should respond with 200', async () => {
-      const response = await request(server).get('/');
-      expect(response.status).toBe(200);
-    })
-  
+describe('index', () => {
+  it('sets the environment to testing', () => {
+    expect(process.env.DB_ENV).toBe('testing');
   });
+describe('GET /', () => {
+  it('should return 200 OK', () => {
+    
+    return request(server)
+      .get('/')
+      .expect(200);
+  });
+
+  it('using the squad (async/await)', async () => {
+ 
+    const res = await request(server).get('/');
+    expect(res.status).toBe(200);
+  });
+
+  it('should return JSON using done callback', done => {
+   
+    request(server)
+      .get('/')
+      .then(res => {
+        expect(res.type).toBe('application/json'); 
+        done();
+      });
+  });
+});
